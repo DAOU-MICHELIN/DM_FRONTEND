4dc15c576f1adec12ee15308ce508b76ff8a885f
myeongju-kim <mjoo1106@naver.com>
2023-12-20 09:24:32 +0900

    feat: vue3, router 세팅

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <img alt="Vue logo" src="./assets/images/logo.png" />
+    <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/common/HelloWorld.vue';
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+};
+</script>
