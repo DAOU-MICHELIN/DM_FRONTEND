8c07652bfb63edab80be6e3838922c5256e3253e
myeongju-kim <mjoo1106@naver.com>
2023-12-28 18:32:57 +0900

    feat: 홈, 로그인, 회원가입 화면 라우팅 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
 import { createWebHistory, createRouter } from 'vue-router';
+import HomeView from '../view/Home';
+import LoginView from '../view/Login';
+import SignUpView from '../view/SignUp';
+
 const routes = [
-    // {
-    //     path: '/',
-    //     name: 'Login',
-    //     component: Login
-    // }
+    {
+        path: '/',
+        name: 'HOME PAGE',
+        component: HomeView,
+    },
+    {
+        path: '/login',
+        name: 'LOGIN PAGE',
+        component: LoginView,
+    },
+    {
+        path: '/account',
+        name: 'SIGN UP PAGE',
+        component: SignUpView,
+    },
 ];
 
 const router = createRouter({
