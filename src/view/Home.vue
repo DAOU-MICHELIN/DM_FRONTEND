121f8b573bafab7ec5675cbe4deca5620bffa100
myeongju-kim <mjoo1106@naver.com>
2024-01-03 15:25:48 +0900

    feat: 가장 처음화면에 지도 노출 (TODO: Naver Maps로 변경)

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,16 +1,38 @@
 <template>
-    <h1>HOME VIEW</h1>
+    <div id="map"></div>
 </template>
 
 <script>
 export default {
     name: 'HomeView',
+    mounted() {
+        window.kakao && window.kakao.maps ? this.loadMap() : this.loadScript();
+    },
+    methods: {
+        loadScript() {
+            const script = document.createElement('script');
+            const appKey = process.env.VUE_APP_KAKAO_CLIENT_KEY;
+
+            script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${appKey}`;
+            script.onload = () => window.kakao.maps.load(this.loadMap);
+            document.head.appendChild(script);
+        },
+        loadMap() {
+            const container = document.getElementById('map');
+            const options = {
+                center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+                level: 3,
+            };
+
+            this.map = new window.kakao.maps.Map(container, options);
+        },
+    },
 };
 </script>
 
 <style scoped>
-h1 {
-    color: gray;
-    font-size: 70px;
+#map {
+    width: 60%;
+    height: 100%;
 }
 </style>
